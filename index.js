123d508bf743ce4449898e59e68118880588d279
michael furst <michael@furst.us>
2018-06-05 00:32:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var i = 0; i < musicians.length; i++)
+  array.push(musicians[i] + " plays " + instruments[i]);
+
+  return array
+}
+
+function johnLennonFacts (facts){
+  var newarray = []
+  var i = 0
+  while (i < facts.length;) {
+    newarray.push (facts[i] + "!!!")
+    i++
+  }
+  return newarray
+}
